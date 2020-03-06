b22e24261b2e4693ef97370530d9d0529b8a1892
Thomas Witt <tom.89@gmx.at>
2020-03-06 10:43:18 +0100

    Und es wurde gemethodifiziert!

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -4,20 +4,27 @@ import java.util.Scanner;
 public class InsertionSort {
     public static void main(String[] args) {
         int[] list = new int[0];
-        int input;
-        System.out.println("Bitte gib die zu sortierenden Zahlen ein:");
         Scanner scanner = new Scanner(System.in);
+        list = giveMeAllYourNumbers(list, scanner);
+        System.out.println("Dein Input:" + Arrays.toString(list));
+        System.out.println(" Sortiert: " + Arrays.toString(itSorts(list)));
+    }
 
+    private static int[] giveMeAllYourNumbers(int[] list, Scanner scanner) {
+        System.out.println("Bitte gib die zu sortierenden Zahlen ein:");
+        int input;
         while (!scanner.hasNext("q") && !scanner.hasNext("Q")) {
-
             input = scanner.nextInt();
             int[] temp = new int[list.length + 1];
             System.arraycopy(list, 0, temp, 0, list.length);
             temp[list.length] = input;
             list = temp;
         }
-        System.out.println("Dein Input:" + Arrays.toString(list));
-        for (int n = 0; n < (list.length-1); n++) {
+        return list;
+    }
+
+    private static int[] itSorts(int[] list) {
+        for (int n = 0; n < (list.length - 1); n++) {
             if (list[n + 1] < list[n]) {
                 for (int i = 0; i < (n + 1); i++) {
                     if (list[n + 1] < list[i]) {
@@ -25,13 +32,13 @@ public class InsertionSort {
                         System.arraycopy(list, 0, templist, 0, list.length);
                         templist[i] = list[n + 1];
                         for (int j = (i + 1); j <= (n + 1); j++) {
-                            templist[j] = list[j-1];
+                            templist[j] = list[j - 1];
                         }
                         list = templist;
                     }
                 }
             }
         }
-        System.out.println(" Sortiert: " + Arrays.toString(list));
+        return list;
     }
 }
